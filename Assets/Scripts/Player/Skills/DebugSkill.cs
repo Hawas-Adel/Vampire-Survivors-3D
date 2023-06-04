fcdf2ae1cf54f55fd9e0844f51e138c8c50f4622
Hawas-Adel <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-04 22:50:10 +0300

    formatting

--- Assets/Scripts/Player/Skills/DebugSkill.cs
+++ Assets/Scripts/Player/Skills/DebugSkill.cs
@@ -6,8 +6,8 @@ public class DebugSkill : Skill
 {
 	public override (float normalizedDelay, Action<SkillManager> action)[] SkillCastCallbacks { get; } = new (float normalizedDelay, Action<SkillManager> action)[]
 	{
-		(0f,_=>Debug.Log($"Start Casting | {Time.time}")),
-		(0.5f,_=>Debug.Log($"MidWay Casting | {Time.time}")),
-		(1f,_=>Debug.Log($"End Casting | {Time.time}")),
+		(0f, _ => Debug.Log($"Start Casting | {Time.time}")),
+		(0.5f, _ => Debug.Log($"MidWay Casting | {Time.time}")),
+		(1f, _ => Debug.Log($"End Casting | {Time.time}")),
 	};
 }
