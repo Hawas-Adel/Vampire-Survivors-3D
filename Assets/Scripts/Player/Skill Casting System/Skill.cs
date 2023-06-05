589c203b1209cb804cee2e676f44c6ba260da7ca
Hawas-Adel <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-05 18:29:28 +0300

    Update Skill.cs

--- Assets/Scripts/Player/Skill Casting System/Skill.cs
+++ Assets/Scripts/Player/Skill Casting System/Skill.cs
@@ -4,7 +4,7 @@ public abstract class Skill : ScriptableObject
 {
 	[field: SerializeField, Min(0f)] public float CastDuration { get; private set; } = 1f;
 	[field: SerializeField, Min(0f)] public float Cooldown { get; private set; } = 1f;
-	[field: SerializeField, Min(0f)] public int MaxCharges { get; private set; } = 1;
+	[field: SerializeField, Min(1)] public int MaxCharges { get; private set; } = 1;
 
 	public abstract (float normalizedDelay, System.Action<SkillManager, Vector3> action)[] SkillCastCallbacks { get; }
 
