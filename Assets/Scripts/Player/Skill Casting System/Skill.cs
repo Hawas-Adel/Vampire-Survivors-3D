60e750ea8a6c8418415725a2e56ee32927f4720e
Hawas-Adel <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-05 12:18:34 +0300

    basic attack (deals no damage)

--- Assets/Scripts/Player/Skill Casting System/Skill.cs
+++ Assets/Scripts/Player/Skill Casting System/Skill.cs
@@ -7,4 +7,11 @@ public abstract class Skill : ScriptableObject
 	[field: SerializeField, Min(0f)] public int MaxCharges { get; private set; } = 1;
 
 	public abstract (float normalizedDelay, System.Action<SkillManager, Vector3> action)[] SkillCastCallbacks { get; }
+
+	protected static Vector3 GetCastDirection(SkillManager caster, Vector3 targetPoint) => (targetPoint - caster.transform.position).normalized;
+	protected static Vector3 GetPointInCastDirection(SkillManager caster, Vector3 targetPoint, float distance)
+	{
+		var dir = GetCastDirection(caster, targetPoint);
+		return caster.transform.position + (dir * distance);
+	}
 }
---
+++ Assets/Scripts/Player/Skills/BasicAttack.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skills/Basic Attack")]
+public class BasicAttack : Skill
+{
+	[field: SerializeField, Min(0f)] public float Range { get; private set; } = 1.5f;
+	[field: SerializeField, Min(0f)] public float AttackVerticalOffset { get; private set; } = 1.5f;
+
+	public override (float normalizedDelay, Action<SkillManager, Vector3> action)[] SkillCastCallbacks { get; }
+
+	public BasicAttack()
+	{
+		SkillCastCallbacks = new (float normalizedDelay, Action<SkillManager, Vector3> action)[]
+		{
+			(0.5f, PerformAttack),
+		};
+	}
+
+	private void PerformAttack(SkillManager caster, Vector3 targetPoint)
+	{
+		Vector3 attackCenter = GetPointInCastDirection(caster, targetPoint, Range / 2f);
+		attackCenter += AttackVerticalOffset * Vector3.up;
+		foreach (var item in Physics.OverlapSphere(attackCenter, Range / 2f))
+		{
+			Debug.Log(item, item);
+		}
+
+		#region Visuals
+		Transform sphereVisual = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+		Destroy(sphereVisual.GetComponent<Collider>());
+		sphereVisual.position = attackCenter;
+		sphereVisual.localScale = Range / 2f * Vector3.one;
+		Destroy(sphereVisual.gameObject, 0.1f);
+		#endregion
+	}
+}
