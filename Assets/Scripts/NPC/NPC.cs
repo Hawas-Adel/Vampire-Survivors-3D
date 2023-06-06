2552a382835e9023ebac26ba47f131285eb948a4
Hawas-Adel <hawas_me@yahoo.com>
2023-06-06 13:07:03 +0300

    refactoring

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public class NPC : MonoBehaviour, IStatsHolder, ITargetable
 {
 	public StatsHandler StatsHandler { get; private set; }
+
 	string ITargetable.TeamID { get; } = nameof(NPC);
 }
