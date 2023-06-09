6522fa222dd0cf27ad2b7ea1e611787e6d36848e
HawasNomad <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-09 19:24:57 +0300

    Targetable Behavior Override system

--- Assets/Scripts/Targeting System/ITargetable.cs
+++ Assets/Scripts/Targeting System/ITargetable.cs
@@ -1,4 +1,21 @@
 public interface ITargetable
 {
 	public string TeamID { get; }
+
+	public void ApplyHitBehavior(System.Action<ITargetable> onHitAction)
+	{
+		if (this is ITargetableBehaviorOverride targetableBehaviorOverride)
+		{
+			targetableBehaviorOverride.OnOverrideHitBehavior(onHitAction);
+		}
+		else
+		{
+			onHitAction.Invoke(this);
+		}
+	}
+}
+
+public interface ITargetableBehaviorOverride : ITargetable
+{
+	public void OnOverrideHitBehavior(System.Action<ITargetable> originalOnHitAction);
 }
