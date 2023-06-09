9d5693ce7f67105d6048ccce7a54b357ecdd9bcb
HawasNomad <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-09 17:37:17 +0300

    Player floating Health & Mana Bar

--- Assets/Scripts/Framework/UI/Character Floating UI/PlayerFloatingUI.cs
+++ Assets/Scripts/Framework/UI/Character Floating UI/PlayerFloatingUI.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerFloatingUI : MonoBehaviour
+public class PlayerFloatingUI : UIElement<IEntity>
 {
+	[SerializeField] private Image HealthBarFillImage;
+	[SerializeField] private Image ManaBarFillImage;
 
+	private void Start() => Bind(GetComponentInParent<IEntity>());
+
+	protected override void _Bind(IEntity value)
+	{
+		StatWithCurrentValue healthStat = BoundValue.StatsHandler.GetStat<StatWithCurrentValue>(StatID._Health);
+		healthStat.OnCurrentValueChanged += UpdateHealthBarFill;
+		UpdateHealthBarFill(healthStat);
+
+		StatWithCurrentValue manaStat = BoundValue.StatsHandler.GetStat<StatWithCurrentValue>(StatID._Mana);
+		manaStat.OnCurrentValueChanged += UpdateManaBarFill;
+		UpdateManaBarFill(manaStat);
+	}
+
+	private void UpdateHealthBarFill(StatWithCurrentValue stat) => HealthBarFillImage.fillAmount = stat.CurrentValue01;
+	private void UpdateManaBarFill(StatWithCurrentValue stat) => ManaBarFillImage.fillAmount = stat.CurrentValue01;
 }
