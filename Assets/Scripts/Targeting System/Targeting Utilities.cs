3b995c1dc3ed3d5bac5db73d12f2e92efa77c244
HawasNomad <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-09 19:27:38 +0300

    rename

--- Assets/Scripts/Targeting System/Targeting Utilities.cs
+++ Assets/Scripts/Targeting System/Targeting Utilities.cs
@@ -5,13 +5,13 @@ public static class TargetingUtilities
 {
 	public static bool AreAllies(ITargetable entity1, ITargetable entity2) => entity1.TeamID == entity2.TeamID;
 
-	private static ITargetable[] GetTargetable(Collider[] colliders) => colliders.
+	private static ITargetable[] GetTargets(Collider[] colliders) => colliders.
 		Select(collider => collider.GetComponentInParent<ITargetable>()).
 		Distinct().
 		Where(targetable => targetable is not null).
 		ToArray();
 
-	public static ITargetable[] GetTargetableEntities(Collider[] colliders, params ITargetable[] IgnoredTargetable) => GetTargetable(colliders).
+	public static ITargetable[] GetTargetableEntities(Collider[] colliders, params ITargetable[] IgnoredTargetable) => GetTargets(colliders).
 		Where(targetable1 => IgnoredTargetable.
 			Any(targetable2 => !AreAllies(targetable1, targetable2))).
 		ToArray();
