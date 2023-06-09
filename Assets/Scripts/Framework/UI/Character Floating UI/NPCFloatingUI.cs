35bd5595a0bcc66f0f72746cb8e8ca309c829897
HawasNomad <36897875+Hawas-Adel@users.noreply.github.com>
2023-06-09 15:56:26 +0300

    enemy name on floating bar

--- Assets/Scripts/Framework/UI/Character Floating UI/NPCFloatingUI.cs
+++ Assets/Scripts/Framework/UI/Character Floating UI/NPCFloatingUI.cs
@@ -1,15 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NPCFloatingUI : UIElement<IEntity>
 {
+	[SerializeField] private TextMeshProUGUI NameText;
 	[SerializeField] private Image HealthBarFillImage;
 
 	private void Start() => Bind(GetComponentInParent<IEntity>());
 
 	protected override void _Bind(IEntity value)
 	{
-		var healthStat = BoundValue.StatsHandler.GetStat<StatWithCurrentValue>(StatID._Health);
+		NameText.text = (value as MonoBehaviour).name;
+		StatWithCurrentValue healthStat = BoundValue.StatsHandler.GetStat<StatWithCurrentValue>(StatID._Health);
 		healthStat.OnCurrentValueChanged += UpdateHealthBarFill;
 		UpdateHealthBarFill(healthStat);
 	}
