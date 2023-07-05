464c4a0f23e86711cafecfac31e497bba453855b
Hawas-Adel <36897875+Hawas-Adel@users.noreply.github.com>
2023-07-05 16:54:26 +0300

    GetTarget utility

--- Assets/Scripts/Targeting System/Targeting Utilities.cs
+++ Assets/Scripts/Targeting System/Targeting Utilities.cs
@@ -6,6 +6,18 @@ public static class TargetingUtilities
 {
 	public static bool AreAllies(ITargetable entity1, ITargetable entity2) => entity1.TeamID == entity2.TeamID;
 
+	private static ITargetable GetTarget(Collider collider) => collider.GetComponentInParent<ITargetable>();
+	public static ITargetable GetTarget(Collider collider, params ITargetable[] ignoredTargets)
+	{
+		ITargetable target = GetTarget(collider);
+		if (ignoredTargets.Any(ignoredTarget => AreAllies(target, ignoredTarget)))
+		{
+			return null;
+		}
+
+		return target;
+	}
+
 	private static ITargetable[] GetTargets(Collider[] colliders) => colliders.
 		Select(collider => collider.GetComponentInParent<ITargetable>()).
 		Distinct().
