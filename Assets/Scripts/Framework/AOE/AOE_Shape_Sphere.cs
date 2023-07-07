5cfa98f75337084f94ef04fc37d74a778406a0da
Hawas-Adel <36897875+Hawas-Adel@users.noreply.github.com>
2023-07-07 18:09:59 +0300

    switche to the NonAlloc version of OverlapSphere

--- Assets/Scripts/Framework/AOE/AOE_Shape_Sphere.cs
+++ Assets/Scripts/Framework/AOE/AOE_Shape_Sphere.cs
@@ -5,12 +5,20 @@ public class AOE_Shape_Sphere : AOE_Shape
 	private readonly Vector3 center;
 	private readonly float radius;
 
+	private readonly Collider[] colliders;
+
 	public AOE_Shape_Sphere(Vector3 center, float radius)
 	{
 		this.center = center;
 		this.radius = radius;
+		colliders = new Collider[0];
 	}
 
-	public override Collider[] GetCollidersInShape(AOE aoe) => Physics.OverlapSphere(aoe.transform.TransformPoint(center), radius);
+	public override Collider[] GetCollidersInShape(AOE aoe)
+	{
+		Physics.OverlapSphereNonAlloc(aoe.transform.TransformPoint(center), radius, colliders);
+		return colliders;
+	}
+
 	public override void DrawGizmos(AOE aoe) => Gizmos.DrawWireSphere(aoe.transform.TransformPoint(center), radius);
 }
